afe562d8f503347af5cdf19c6dec03dd2aa877ee
Enmeng Liu <enmeng.liu@mail.utoronto.ca>
2022-09-06 21:13:30 -0400

    replace all the hard-coded links with the new hard-coded ones

--- Education_Pathways/frontend/src/components/ResultDisplay.jsx
+++ Education_Pathways/frontend/src/components/ResultDisplay.jsx
@@ -28,7 +28,7 @@ class SearchResultDisplay extends Component{
   }
 
   getData = (input) => {
-    axios.get(`https://astarchooser.herokuapp.com/searchc?input=${input}`)
+    axios.get(`https://assignment-1-starter-template.herokuapp.com/searchc?input=${input}`)
       .then(res => {
         console.log(`it is ${res.status}`)
         if (res.status === 200) {
