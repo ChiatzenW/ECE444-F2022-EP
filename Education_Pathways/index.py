c797af26f4a2fa8d90e1920130edb79b81e2e7e4
Enmeng Liu <enmeng.liu@mail.utoronto.ca>
2022-09-27 20:05:16 -0400

    rewrite index.py

--- Education_Pathways/index.py
+++ Education_Pathways/index.py
@@ -25,104 +25,104 @@ config.init_cors(app)
 
 # rount functions
 
-def search_course_by_code(s):
-    # return all the courses whose course code contains the str s
-    course_ids = df[df['Code'].str.contains(s.upper())].index.tolist()
-    if len(course_ids) == 0:
-        return []
-    if len(course_ids) > 10:
-        course_ids = course_ids[:10]
-    res = []
-    for i, course_id in enumerate(course_ids):
-        d = df.iloc[course_id].to_dict()
-        res_d = {
-            '_id': i,
-            'code': d['Code'],
-            'name': d['Name'],
-            'description': "Course description here.",
-            'syllabus': "Course syllabus here.",
-            'prereq': ['APS101H1, ECE101H1'],
-            'coreq': ['APS102H1, ECE102H1'],
-            'exclusion': ['APS102H1, ECE102H1'] ,
-        }
-        res.append(res_d)
-    return res
+# def search_course_by_code(s):
+#     # return all the courses whose course code contains the str s
+#     course_ids = df[df['Code'].str.contains(s.upper())].index.tolist()
+#     if len(course_ids) == 0:
+#         return []
+#     if len(course_ids) > 10:
+#         course_ids = course_ids[:10]
+#     res = []
+#     for i, course_id in enumerate(course_ids):
+#         d = df.iloc[course_id].to_dict()
+#         res_d = {
+#             '_id': i,
+#             'code': d['Code'],
+#             'name': d['Name'],
+#             'description': "Course description here.",
+#             'syllabus': "Course syllabus here.",
+#             'prereq': ['APS101H1, ECE101H1'],
+#             'coreq': ['APS102H1, ECE102H1'],
+#             'exclusion': ['APS102H1, ECE102H1'] ,
+#         }
+#         res.append(res_d)
+#     return res
 
-class SearchCourse(Resource):
-    def get(self):
-        input = request.args.get('input')
-        courses = search_course_by_code(input)
-        # courses =[{'_id': 1, 'code': 'ECE444', 'name': 'SE'}, {'_id': 2,'code': 'ECE333', 'name': 'ur mom'}]
-        if len(courses) > 0:
-            try:
-                resp = jsonify(courses)
-                resp.status_code = 200
-                return resp
-            except Exception as e:
-                resp = jsonify({'error': str(e)})
-                resp.status_code = 400
-                return resp
+# class SearchCourse(Resource):
+#     def get(self):
+#         input = request.args.get('input')
+#         courses = search_course_by_code(input)
+#         # courses =[{'_id': 1, 'code': 'ECE444', 'name': 'SE'}, {'_id': 2,'code': 'ECE333', 'name': 'ur mom'}]
+#         if len(courses) > 0:
+#             try:
+#                 resp = jsonify(courses)
+#                 resp.status_code = 200
+#                 return resp
+#             except Exception as e:
+#                 resp = jsonify({'error': str(e)})
+#                 resp.status_code = 400
+#                 return resp
 
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('input', required=True)
-        data = parser.parse_args()
-        input = data['input']
-        courses = search_course_by_code(input)
-        if len(courses) > 0:
-            try:
-                resp = jsonify(courses)
-                resp.status_code = 200
-                return resp
-            except Exception as e:
-                resp = jsonify({'error': 'something went wrong'})
-                resp.status_code = 400
-                return resp
+#     def post(self):
+#         parser = reqparse.RequestParser()
+#         parser.add_argument('input', required=True)
+#         data = parser.parse_args()
+#         input = data['input']
+#         courses = search_course_by_code(input)
+#         if len(courses) > 0:
+#             try:
+#                 resp = jsonify(courses)
+#                 resp.status_code = 200
+#                 return resp
+#             except Exception as e:
+#                 resp = jsonify({'error': 'something went wrong'})
+#                 resp.status_code = 400
+#                 return resp
 
-class ShowCourse(Resource):
-    def get(self):
-        code = request.args.get('code')
-        courses = search_course_by_code(code)
-        if len(courses) == 0:
-            resp = jsonify({'message': f"Course {code} doesn't exist"})
-            resp.status_code = 404
-            return resp
-        try:
-            resp = jsonify({'course': courses[0]})
-            resp.status_code = 200
-            return resp
-        except Exception as e:
-            resp = jsonify({'error': 'something went wrong'})
-            resp.status_code = 400
-            return resp
+# class ShowCourse(Resource):
+#     def get(self):
+#         code = request.args.get('code')
+#         courses = search_course_by_code(code)
+#         if len(courses) == 0:
+#             resp = jsonify({'message': f"Course {code} doesn't exist"})
+#             resp.status_code = 404
+#             return resp
+#         try:
+#             resp = jsonify({'course': courses[0]})
+#             resp.status_code = 200
+#             return resp
+#         except Exception as e:
+#             resp = jsonify({'error': 'something went wrong'})
+#             resp.status_code = 400
+#             return resp
     
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('code', required=True)
-        data = parser.parse_args()
-        code = data['code']
-        courses = search_course_by_code(code)
-        if len(courses) == 0:
-            resp = jsonify({'message': f"Course {code} doesn't exist"})
-            resp.status_code = 404
-            return resp
-        try:
-            resp = jsonify({'course': courses[0]})
-            resp.status_code = 200
-            return resp
-        except Exception as e:
-            resp = jsonify({'error': 'something went wrong'})
-            resp.status_code = 400
-            return resp
+#     def post(self):
+#         parser = reqparse.RequestParser()
+#         parser.add_argument('code', required=True)
+#         data = parser.parse_args()
+#         code = data['code']
+#         courses = search_course_by_code(code)
+#         if len(courses) == 0:
+#             resp = jsonify({'message': f"Course {code} doesn't exist"})
+#             resp.status_code = 404
+#             return resp
+#         try:
+#             resp = jsonify({'course': courses[0]})
+#             resp.status_code = 200
+#             return resp
+#         except Exception as e:
+#             resp = jsonify({'error': 'something went wrong'})
+#             resp.status_code = 400
+#             return resp
 
 
 # API Endpoints
 import controller
 rest_api = Api(app)
-# rest_api.add_resource(controller.SearchCourse, '/searchc')
-rest_api.add_resource(SearchCourse, '/searchc')
-# rest_api.add_resource(controller.ShowCourse, '/course/details')
-rest_api.add_resource(ShowCourse, '/course/details')
+rest_api.add_resource(controller.SearchCourse, '/searchc')
+# rest_api.add_resource(SearchCourse, '/searchc')
+rest_api.add_resource(controller.ShowCourse, '/course/details')
+# rest_api.add_resource(ShowCourse, '/course/details')
 
 
 @app.route("/", defaults={'path': ''})
